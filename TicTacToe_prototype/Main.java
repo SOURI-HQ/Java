9882bc306fc989cbe09d15945f538422591132d8
SOURI-HQ <seconddan@wp.pl>
2020-08-07 19:37:01 +0200

    Minor code simplifying

--- TicTacToe_prototype/Main.java
+++ TicTacToe_prototype/Main.java
@@ -1,9 +1,8 @@
 package tictactoe;
-
 import java.util.Scanner;
 
 public class Main {
-    public static void printBoard (char[][] board) {
+    public static void printField (char[][] board) {
         System.out.println("---------");
         for (int i = 0; i < 3; i++) {
             System.out.print("|");
@@ -15,8 +14,7 @@ public class Main {
         System.out.println("---------");
     }
 
-
-    public static boolean isInteger (String input) {
+    public static boolean isInputCorrect (String input) {
         if (input.matches("[0-9]")) {
             if (input.matches("[1-3]")) {
                 return true;
@@ -32,83 +30,73 @@ public class Main {
         }
     }
 
-    public static char nextMoveChar (String cells) {
+    public static char whatIsNextSymbol (String cells) {
         int numberOfX = 0, numberOfO = 0;
         for (int i = 0; i < cells.length(); i++) {
-            if (cells.charAt(i) == 'X') {
-                numberOfX++;
-            }
-            else if (cells.charAt(i) == 'O') {
-                numberOfO++;
-            }
+            if (cells.charAt(i) == 'X') numberOfX++;
+            else if (cells.charAt(i) == 'O') numberOfO++;
         }
-        if (numberOfX == numberOfO || numberOfX < numberOfO) {
-            return 'X';
-        }
+        if (numberOfX == numberOfO || numberOfX < numberOfO) return 'X';
+
         else return 'O';
     }
 
-    public static String stateOfGame (char[][] board) {
-        int j;
+    public static String getResults (char[][] board) {
+        int j = 0;
         for (int i = 0; i < 3; i++) {
-            j = 0;
-            if (board[i][j] != ' ' && board[i][j] == board[i][j+1] && board[i][j+1] == board[i][j+2]) {
+            // horizontally
+            if (board[i][j] != ' ' && board[i][j] == board[i][j+1] && board[i][j+1] == board[i][j+2])
                 return board[i][j] + " wins";
-
-            }
-            if (board[j][i] != ' ' && board[j][i] == board[j+1][i] && board[j][i] == board[j+2][i]) {
+            // vertically
+            if (board[j][i] != ' ' && board[j][i] == board[j+1][i] && board[j][i] == board[j+2][i])
                 return board[j][i] + " wins";
-            }
         }
-        if (board[1][1] != ' ' && board[0][0] == board[1][1] && board [1][1] == board[2][2] || board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+        // diagonally
+        if (board[1][1] != ' ' && board[0][0] == board[1][1] && board [1][1] == board[2][2] || board[0][2] == board[1][1] && board[1][1] == board[2][0])
             return board[1][1] + " wins";
-        }
+
         for (int i = 0; i < 3; i++) {
             for (j = 0; j < 3; j++) {
                 if (board[i][j] == ' ') return "Game not finished";
             }
         }
         return "Draw";
-
     }
 
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
+        char[][] field = new char[3][3];
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter cells: ");
         String cells = scanner.next().replace("_", " ");
-        int counter = 0;
-        int xInt, yInt;
-        String x = null, y = null;
+        int xInt, yInt, counter = 0;
+        String x, y;
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                board[i][j] = cells.charAt(counter++);
+                field[i][j] = cells.charAt(counter++);
             }
         }
-        printBoard(board);
+        printField(field);
 
-        boolean correctInput = false;
-        while (!correctInput) {
+        while (true) {
+            scanner.nextLine(); // taking scanner stream to the next line in order to omit last user's input
             System.out.print("Enter the coordinates: ");
             x = scanner.next();
-            correctInput = isInteger(x);
-            if(correctInput) {
+            if (isInputCorrect(x)) {
                 y = scanner.next();
-                correctInput = isInteger(y);
             }
-            if (correctInput) {
+            else continue; // skipping current iteration so isInputCorrect won't throw NullPointerException
+            if (isInputCorrect(y)) {
                 xInt = Integer.parseInt(x) - 1;
                 yInt = Integer.parseInt(y);
                 yInt = yInt == 1 ? 2 : yInt == 3 ? 0 : 1;
-                if (board[yInt][xInt] != ' ') {
-                    System.out.println("This cell is occupied! Choose another one!");
-                    correctInput = false;
+                if (field[yInt][xInt] == ' ') {
+                    field[yInt][xInt] = whatIsNextSymbol(cells);
+                    printField(field);
+                    System.out.println(getResults(field));
                 }
-                if (correctInput) {
-                    board[yInt][xInt] = nextMoveChar(cells);
-                    printBoard(board);
-                    System.out.println(stateOfGame(board));
-                }
+                else { System.out.println("This cell is occupied! Choose another one!"); continue; }
+                break;
             }
         }
     }
